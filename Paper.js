35140979f4ce0275512b305a03b43b25ba50dca0
Zahraa2345 <68749331+Zahraa2345@users.noreply.github.com>
2020-08-21 15:16:00 -0400

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,32 @@
+class Paper{
+
+ constructor(x, y, r){
+
+    var options= {
+      isStatic: false,
+      restitution: 0.8,
+      friction: 1,
+      density: 1.2,
+    }
+
+    this.body=Bodies.circle(x, y, r, options);
+    this.radius= r;
+   // Matter.Body.setAngle(this.body, angle);
+    World.add(world,this.body);
+
+ }
+
+ display(){
+    
+    var pos= this.body.position;
+    var ang= this.body.angle
+    push();
+    translate(pos.x,pos.y);
+    rotate(ang)
+    ellipseMode(RADIUS);
+    fill("blue");
+    ellipse(0, 0, this.radius, this.radius);
+    pop();
+    }
+
+ }
